f4d790191cbeb6c1d6225b3d3aa11a6eaf6f80e8
henryho77 <squirrelho@gmail.com>
2014-05-30 15:36:16 +0800

    add a button click debug

--- 20140530/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140530/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -45,6 +46,10 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+	public void send(View view) {
+ 		Log.d("debug", "click");
+ 	}
 
     /**
      * A placeholder fragment containing a simple view.
