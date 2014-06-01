7e855081476f9fffed428733aed648b8eb49733a
henryho77 <squirrelho@gmail.com>
2014-06-01 17:33:22 +0800

    add checkbox

--- 20140530/SimpleUI/src/com/example/simpleui/MainActivity.java
+++ 20140530/SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.os.Build;
@@ -59,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
 
     	private Button button;
     	private EditText editText;
-    	
+    	private CheckBox checkBox;
     	
         public PlaceholderFragment() {
         }
@@ -71,7 +72,7 @@ public class MainActivity extends ActionBarActivity {
             
             button = (Button) rootView.findViewById(R.id.button1);
             editText = (EditText) rootView.findViewById(R.id.editText1);
-            
+            checkBox = (CheckBox) rootView.findViewById(R.id.checkBox1);
             
             button.setOnClickListener(new OnClickListener(){
 
@@ -89,6 +90,10 @@ public class MainActivity extends ActionBarActivity {
         
 
 		private void showToast(String text) {
+			if(checkBox.isChecked()){
+				text = "***********";
+			}
+			
  			Toast.makeText(getActivity(), text, Toast.LENGTH_LONG).show();
  		}        
     }
