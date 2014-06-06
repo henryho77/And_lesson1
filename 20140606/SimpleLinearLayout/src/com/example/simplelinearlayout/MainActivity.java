71063d38a01660b9cd0d442cc15fc77e52dfead4
henryho77 <squirrelho@gmail.com>
2014-06-06 10:12:43 +0800

    add a new project SimpleLinearLayout

--- 20140606/SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
+++ 20140606/SimpleLinearLayout/src/com/example/simplelinearlayout/MainActivity.java
@@ -1,22 +1,14 @@
-package com.example.simpleui;
+package com.example.simplelinearlayout;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -53,19 +45,12 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    
-    	public void send(View view) {
-    	  		Log.d("debug", "click");
-    	  	}
+
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
 
-    	private Button button;
-    	private EditText editText;
-    	private CheckBox checkBox;
-    	
         public PlaceholderFragment() {
         }
 
@@ -73,51 +58,8 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            
-            button = (Button) rootView.findViewById(R.id.button1);
-            editText = (EditText) rootView.findViewById(R.id.editText1);
-            checkBox = (CheckBox) rootView.findViewById(R.id.checkBox1);
-            
-            button.setOnClickListener(new OnClickListener(){
-
-				@Override
- 				public void onClick(View v) 
-				{
-					showToast(editText.getText().toString());
- 				}	
-            	
-            });
-            
-			editText.setOnKeyListener(new OnKeyListener()
-			{
-	 
-	 				@Override
-	 				public boolean onKey(View v, int keyCode, KeyEvent event)
-	 				{
-	 					if (event.getAction() == KeyEvent.ACTION_DOWN) 
-	 					{
-	 						if (keyCode == KeyEvent.KEYCODE_ENTER) 
-	 						{
-	 							showToast(editText.getText().toString());
-	 							return true;
-	 						}
-	 					}
-	 					return false;
-	 				}	
-	 		});            
-            
-            
             return rootView;
         }
-        
-
-		private void showToast(String text) {
-			if(checkBox.isChecked()){
-				text = "***********";
-			}
-			
- 			Toast.makeText(getActivity(), text, Toast.LENGTH_LONG).show();
- 		}        
     }
 
 }
